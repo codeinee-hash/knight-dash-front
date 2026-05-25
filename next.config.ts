b18669e3ca8e15af818c6d0126414859357e5005
codeinee <>
2026-05-25 21:24:30 +0600

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
   images: {
     remotePatterns: [
+      { protocol: 'https', hostname: 'knight-dash-back.up.railway.app' },
       { protocol: 'http', hostname: 'localhost' },
-      { protocol: 'http', hostname: '127.0.0.1' },
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' },
     ],
   },
 }
